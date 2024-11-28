1fbcb81840d94ac2255a14002c01527f3564f68c
pandalee714 <lpengda225@gmail.com>
2024-11-28 03:58:22 +0000

    2

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    return(render_template("index2.html"))
+    return(render_template("index.html"))
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=5001)
