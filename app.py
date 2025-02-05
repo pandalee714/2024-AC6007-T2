500d9dc8f12fbec4577ae4b2b4b7530f3e763e11
pandalee714 <lpengda225@gmail.com>
2025-02-05 03:35:51 +0000

    1

--- app.py
+++ app.py
@@ -20,5 +20,9 @@ def SA_result():
     r = textblob.TextBlob(q).sentiment
     return(render_template("SA_result.html",r=r))
 
+@app.route("/paynow",methods=["GET","POST"])
+def paynow():
+    return(render_template("paynow.html"))
+
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=5001)
